b343428090fb9c9e1b394cba59e5904c9f3e2c9f
Luiz Fernando da Silva Borges <40524905+luizfsborges@users.noreply.github.com>
2023-02-28 12:16:42 -0300

    [modularização do firmware do sensor de distância infravermelho para pull request]

--- src/embedded/raspberry_sensor_iv_serial.py
+++ src/embedded/raspberry_sensor_iv_serial.py
@@ -2,12 +2,28 @@ import sys
 from time import sleep
 import machine
 
-entrada_sensor_iv = machine.ADC(28)
+def le_distancia(pino_sensor_iv):
+    
+    leitura_analogica_sensor_iv = pino_sensor_iv.read_u16()
+    leitura_sensor_iv = int(str(leitura_analogica_sensor_iv)[:3]) #digitos significativos para nosso range de distância
+    distancia = -4.287e-7*(leitura_sensor_iv**3) + 0.0004954*(leitura_sensor_iv**2) - 0.2097*leitura_sensor_iv + 38.21
+    distancia_ajustada = round(distancia, 2) - 1 # -1 é o offset do suporte do sensor
+    
+    return distancia_ajustada
 
+def escreve_serial(id_sensor, leitura_numerica):
+    
+    string_mensagem = id_sensor + ":" + str(leitura_numerica)
+    sys.stdout.write(string_mensagem)
+    
+    return string_mensagem
+
+entrada_sensor_iv = machine.ADC(28)
+    
 while True:
     
-    leitura_sensor_iv = entrada_sensor_iv.read_u16()
+    distancia = le_distancia(entrada_sensor_iv)
     
-    print("Leitura analógica", leitura_sensor_iv)
-    sys.stdout.write(leitura_sensor_iv) #Escreve na serial
-    sleep(2)
+    escreve_serial("iv", distancia)
+    print("\n")
+    sleep(0.5)
