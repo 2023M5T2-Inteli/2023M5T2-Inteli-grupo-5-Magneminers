8727d00f256c4b34f0624872c46a15bf296b1c0e
Joao Pedro Goncalves Carazzato <99187756+joaocarazzato@users.noreply.github.com>
2023-03-13 07:14:33 -0300

    backend_iv_e_eletroima.py, codigo mais robusto
    Adicionando variáveis especiais do Python a fim de deixar o código um pouco mais robusto e além disso, evitando erros. Criando também uma função nova para diminuir um pouco os códigos de envios do eletroímã e do sensor IV.

--- src/backend/backend_iv_e_eletroima.py
+++ src/backend/backend_iv_e_eletroima.py
@@ -35,51 +35,60 @@ def conecta_serial(porta_com, taxa_transmissao, tempo_espera):
 
     return comunicacao_serial
 
-tempo_espera = 2
-taxa_transmissao = 115200
+# Realiza todo o processo de conexao ate o rasp pela chamada de outras funcoes
+def processo_conexao(taxa_de_transmissao_comunicacao, tempo_de_espera_conexao):
+    
+    portas_com = lista_coms() # Lista todas as portas COM
+    porta_rasp = encontra_rasp(portas_com) # Encontra a porta COM do Raspberry Pi Pico
+    comunicacao_serial = conecta_serial(porta_rasp, taxa_de_transmissao_comunicacao, tempo_de_espera_conexao) # Abre um objeto de comunicação com a porta na qual o Raspberry Pi Pico está
 
-portas_com = lista_coms() # Lista todas as portas COM
-porta_rasp = encontra_rasp(portas_com) # Encontra a porta COM do Raspberry Pi Pico
-comunicacao_serial = conecta_serial(porta_rasp, taxa_transmissao, tempo_espera) # Abre um objeto de comunicação com a porta na qual o Raspberry Pi Pico está
+def enviar_info_ao_rasp(sensor, estado, valor_pwm):
+    #Junta todos em uma string
+    info_sensor = str(sensor) + "," + str(estado) + "," + str(valor_pwm)
+    print(info_sensor)
+    # Codifica o número para que consiga ser enviado ao Raspberry.
+    sensor_encode = info_sensor.encode()
+    # Envia o número de estado para o Raspberry através da linha de comando que definiu as suas propriedades, escrevendo através da serial
+    comunicacao_serial.write(sensor_encode + b"\n") # Escreve o número na serial para o Raspberry receber e fazer a manipulacao atraves da Ponte H.
+
+# Configurações de comunicação
+taxa_transmissao = 115200 # bits por segundo
+tempo_espera = 2 # segundos
+
+# variaveis especiais para deixar o codigo mais robusto e evitar erros
+if __name__ == "__main__":
+    
+    processo_conexao(taxa_transmissao, tempo_espera)
 
 # Inicializa o loop do python de requisição/envio a Serial do Raspberry caso seja conectado e tudo a cima de certo para comecar a manipulacao do eletroima
-while True:
-    
-    sensor_requisitado = input("Sensor: ")
-    
-    if sensor_requisitado == "ec":
-        # Espera o input do usuario para mudar o estado do eletroima
-        estado_eletroima = input("Estado: ")
-        # Transforma em string para depois ser codificado e ser enviado ao Raspberry 
-        estado_eletroima = str(estado_eletroima)
-        # Apresenta o número de estado no terminal do Python
-        print(estado_eletroima)
-        
-        # Valor do PWM editado a partir do Input do usuario.
-        valor_pwm_eletroima = input("Valor PWM: ")
-        valor_pwm_eletroima = str(valor_pwm_eletroima)
-        #Junta ambos em uma string
-        info_eletroima = str(sensor_requisitado) + "," + estado_eletroima + "," + valor_pwm_eletroima
-        print(info_eletroima)
-        # Codifica o número para que consiga ser enviado ao Raspberry.
-        eletroima_encode = info_eletroima.encode()
+    while True:
 
-        # Envia o número de estado para o Raspberry através da linha de comando que definiu as suas propriedades, escrevendo através da serial
-        comunicacao_serial.write(eletroima_encode + b"\n") # Escreve o número na serial para o Raspberry receber e fazer a manipulacao atraves da Ponte H
-        time.sleep(1) # Espera um segundo e repete o loop
-        
-    elif sensor_requisitado == "iv":
-        
-        # Para evitar erros no raspberry ao realizar o split para transformarmos os dados em uma lista, temos que definir dados para o Infravermelho tambem...
-        estado_iv = "1"
-        valor_pwm_iv = "100"
-        
-        info_iv = str(sensor_requisitado) + "," + estado_iv + "," + valor_pwm_iv
-        print(info_iv)
-        
-        iv_encode = info_iv.encode()
-        
-        comunicacao_serial.write(iv_encode + b"\n")
-        #Leitura do Sensor infravermelho conectado no Raspberry Pico W
-        valor_distancia = comunicacao_serial.readline() # Realiza a leitura de um pacote de informaçãoes via serial:
-        print(valor_distancia.decode("utf-8")) # Exibe a string que foi enviada pelo Raspberry Pi Pico decodificada como texto
+        sensor_requisitado = input("Sensor: ")
+
+        if sensor_requisitado == "ec":
+            # Espera o input do usuario para mudar o estado do eletroima
+            estado_eletroima = input("Estado: ")
+            # Transforma em string para depois ser codificado e ser enviado ao Raspberry 
+            estado_eletroima = str(estado_eletroima)
+            # Apresenta o número de estado no terminal do Python
+            print(estado_eletroima)
+
+            # Valor do PWM editado a partir do Input do usuario.
+            valor_pwm_eletroima = input("Valor PWM: ")
+            valor_pwm_eletroima = str(valor_pwm_eletroima)
+            
+            enviar_info_ao_rasp(sensor_requisitado, estado_eletroima, valor_pwm_eletroima)
+            time.sleep(1) # Espera um segundo e repete o loop
+
+        elif sensor_requisitado == "iv":
+
+            # Para evitar erros no raspberry ao realizar o split para transformarmos os dados em uma lista, temos que definir dados para o Infravermelho tambem, sao os numeros padroes porem que fazem grandes diferencas pra gente nesse resultado em especifico(afinal, se ele estiver desligado por exemplo, como vamos pegar os dados de distancia?)...
+            estado_iv = "1"
+            valor_pwm_iv = "100"
+
+            enviar_info_ao_rasp(sensor_requisitado, estado_iv, valor_pwm_iv)
+            
+            #Leitura do Sensor infravermelho conectado no Raspberry Pico W
+            valor_distancia = comunicacao_serial.readline() # Realiza a leitura de um pacote de informaçãoes via serial(a principio, a distancia por ser o sensor IV):
+            print(valor_distancia.decode("utf-8")) # Exibe a string que foi enviada pelo Raspberry Pi Pico decodificada como texto
+            time.sleep(1) # Espera um segundo e repete o loop
