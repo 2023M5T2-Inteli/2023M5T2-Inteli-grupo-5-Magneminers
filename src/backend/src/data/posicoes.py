04c3b47bcab919d0b2ea777b164fa02e8267d3e5
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2023-03-14 16:38:47 -0300

    Atualização do Backend com o Frontend - Sprint 4

---
+++ src/backend/src/data/posicoes.py
@@ -0,0 +1,23 @@
+class Posicao():
+    def __init__(self, x1, y1, z1, xd1, yd1, x2, y2, z2, xd2, yd2, x3, y3, z3, xd3, yd3, ptc, vrr, cic) -> None:
+        self.x1 = x1
+        self.y1 = y1
+        self.z1 = z1
+        self.xd1 = xd1
+        self.yd1 = yd1
+
+        self.x2 = x2
+        self.y2 = y2
+        self.z2 = z2
+        self.xd2 = xd2
+        self.yd2 = yd2
+
+        self.x3 = x3
+        self.y3 = y3
+        self.z3 = z3
+        self.xd3 = xd3
+        self.yd3 = yd3
+
+        self.ptc = ptc
+        self.vrr = vrr
+        self.cic = cic
